6ecd282fb243bada9b6f2edf245b6c59e42c173a
DanielMc <daniel_mckerrocher@ultimatesoftware.com>
2020-10-02 13:12:48 -0400

    Implement Logging
    * Replace calls to `println` with logs
    * Refactor `SchemaUtils#create` to use string template
    * Some stylistic cleanup
    
    Signed-off-by: DeMack <dmckerrocher@gmail.com>

--- mapper/src/main/kotlin/net/lostillusion/lostorm/mapper/operations/Executors.kt
+++ mapper/src/main/kotlin/net/lostillusion/lostorm/mapper/operations/Executors.kt
@@ -1,35 +1,37 @@
 package net.lostillusion.lostorm.mapper.operations
 
+import mu.KotlinLogging
 import net.lostillusion.lostorm.mapper.Entity
-import java.lang.RuntimeException
 import java.sql.Connection
 import java.sql.SQLException
+
+private val LOGGER = KotlinLogging.logger {}
 
 interface OperationExecutor<R> {
     fun execute(generator: StatementGenerator, connection: Connection): R
 }
 
-object SelectCountExecutor: OperationExecutor<Int> {
+object SelectCountExecutor : OperationExecutor<Int> {
     override fun execute(generator: StatementGenerator, connection: Connection): Int {
-        println("Executing query: ${generator.generateStatement()}")
+        LOGGER.debug { "Executing query: ${generator.generateStatement()}" }
         val result = connection.prepareStatement(generator.generateStatement()).executeQuery()
         result.next()
         return result.getInt(1)
     }
 }
 
-class SelectExectutor<D: Any>(private val entity: Entity<D>): OperationExecutor<List<D>> {
+class SelectExectutor<D : Any>(private val entity: Entity<D>) : OperationExecutor<List<D>> {
     override fun execute(generator: StatementGenerator, connection: Connection): List<D> {
-        println("Executing query: ${generator.generateStatement()}")
+        LOGGER.debug { "Executing query: ${generator.generateStatement()}" }
         val result = connection.prepareStatement(generator.generateStatement()).executeQuery()
         val values = mutableListOf<MutableList<Any?>>()
-        while(result.next()) {
+        while (result.next()) {
             val currentResult = mutableListOf<Any?>()
             entity.columns.forEach {
                 try {
                     currentResult += result.getObject(it.columnName)
-                } catch(e: SQLException) {
-                    if(it.nullable) currentResult.add(null)
+                } catch (e: SQLException) {
+                    if (it.nullable) currentResult.add(null)
                     //TODO: Make this a unique exception
                     else throw RuntimeException("No value found for non-nullable column: ${it.columnName}")
                 }
@@ -40,9 +42,9 @@ class SelectExectutor<D: Any>(private val entity: Entity<D>): OperationExecutor<
     }
 }
 
-object UpdateExectuor: OperationExecutor<Int> {
+object UpdateExectuor : OperationExecutor<Int> {
     override fun execute(generator: StatementGenerator, connection: Connection): Int {
-        println("Executing update: ${generator.generateStatement()}")
+        LOGGER.debug { "Executing update: ${generator.generateStatement()}" }
         return connection.prepareStatement(generator.generateStatement()).executeUpdate()
     }
 }

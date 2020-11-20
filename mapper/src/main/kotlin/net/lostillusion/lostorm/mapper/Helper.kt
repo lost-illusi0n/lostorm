15150545cbf0ef432610ec350c056e27c8761286
Lost Illusion <zctrap@gmail.com>
2020-11-20 15:26:19 -0500

    add date time data types

--- mapper/src/main/kotlin/net/lostillusion/lostorm/mapper/Helper.kt
+++ mapper/src/main/kotlin/net/lostillusion/lostorm/mapper/Helper.kt
@@ -1,10 +1,16 @@
 package net.lostillusion.lostorm.mapper
 
 import java.lang.IllegalArgumentException
+import java.sql.Date
+import java.sql.Time
+import java.sql.Timestamp
 
 internal fun toSafeSQL(value: Any?): String {
     return when(value) {
         is String -> "'$value'"
+        is Date -> "'$value'"
+        is Time -> "'$value'"
+        is Timestamp -> "'$value'"
         //Double, Float, Long, Short, and Int should all fall under this
         is Number -> "$value"
         is Boolean -> "$value"
--- mapper/src/main/kotlin/net/lostillusion/lostorm/mapper/SchemaUtils.kt
+++ mapper/src/main/kotlin/net/lostillusion/lostorm/mapper/SchemaUtils.kt
@@ -5,23 +5,19 @@ import mu.KotlinLogging
 private val LOGGER = KotlinLogging.logger {}
 
 object SchemaUtils {
-    @Suppress("NestedLambdaShadowedImplicitParameter")
     fun create(session: Session, vararg entities: Entity<*>) {
         session.connection {
-            entities.forEach {
-                val sql =
-                    """create table if not exists ${it.tableName}
-                        | (${
-                    it.columns.joinToString(", ") {
-                        """${it.columnName}
-                                        | ${DataTypes.kotlinToSQL[it.sqlClass]}
-                                        |${if (it.nullable) "" else " not null"}
-                                        |${if (it.hasDefaultValue) " default ${it.defaultValue}" else ""}
-                                    """.trimMargin()
-                    }
-                    }
-                        |${if (it.primaryKey.asSQL.isNotEmpty()) ", ${it.primaryKey.asSQL}" else ""})
-                    """.trimMargin()
+            entities.forEach { entity ->
+                val sql = "create table if not exists ${entity.tableName} (" +
+                    //add column information
+                    entity.columns.joinToString(", ") { column ->
+                        "${column.columnName} ${DataTypes.kotlinToSQL[column.sqlClass]}" +              //column_name column_type
+                            (if(!column.nullable) " not null " else "") +                               //not null
+                            (if(column.hasDefaultValue) " default ${column.defaultValue} " else "")     //default value
+                    } +
+                    //primary key information
+                    (if(entity.primaryKey.asSQL.isNotEmpty()) ", ${entity.primaryKey.asSQL}" else "") + //primary keys
+                    ")"
                 LOGGER.debug { "Executing call: $sql" }
                 prepareCall(sql).execute()
             }
